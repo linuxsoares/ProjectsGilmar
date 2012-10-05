9278710228425357df512c60780a1812b0ed6da0
linuxsoares <linux.soares@gmail.com>
2012-10-05 13:07:10 -0300

    teste

--- LineSyncNew/src/Connection/ConnectionFactory.java
+++ LineSyncNew/src/Connection/ConnectionFactory.java
@@ -5,7 +5,6 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 	
-	//Variaveis de Configuração
 	private static final String JDBC_DRIVER = "oracle.jdbc.OracleDriver";
 	private static final String URL_CONNECTION = "jdbc:oracle:thin:@10.100.0.20:1521:cmt";  
 	private static final String DB_USER = "gsoares";  
