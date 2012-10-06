162d118b01d80ad3f2f360eac020281e6ff5a174
gilmar <gilmar@192.168.0.197>
2012-10-06 09:29:43 -0700

    Commit

--- LineSyncNew/src/Connection/ConnectionFactory.java
+++ LineSyncNew/src/Connection/ConnectionFactory.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 	
+	//Variaveis Configuração de BD
 	private static final String JDBC_DRIVER = "oracle.jdbc.OracleDriver";
 	private static final String URL_CONNECTION = "jdbc:oracle:thin:@10.100.0.20:1521:cmt";  
 	private static final String DB_USER = "gsoares";  
